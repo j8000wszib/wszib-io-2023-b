132ad0dd7417c10933495d585948f3a16ea38ddb
kirchner <kirchner@student.wszib.edu.pl>
2023-03-19 18:23:52 +0100

    more triangle types

--- trojkaty.java
+++ trojkaty.java
@@ -1,27 +1,41 @@
 /**
-* To jest program do rozpoznawania trójkąta.
-*/
+ * To jest program do rozpoznawania trójkąta.
+ */
 
 class Trojkaty {
     /**
      * Główna fnukcja programu.
+     *
      * @param {float} a - Długość pierwszego boku.
      * @param {float} b - Długość drugiegio boku.
      * @param {float} c - Długość trzeciego boku.
      */
-    public static void jakiTrojkat(float a, float b, float c){
-         if (a == b && b == c && a == c) {
-             System.out.println("Trójkąt równoboczny"); 
-         }
-         // TODO: tutaj trzeba bedzie dopisac inne przypadki
+    public static void jakiTrojkat(float a, float b, float c) {
+        if (a == b && b == c && a == c) {
+            System.out.println("Trójkąt równoboczny");
+        } else if (a * a + b * b == c * c || c * c + b * b == a * a || a * a + c * c == b * b) {
+            System.out.println("Trojkat prostokatny");
+        } else if (a == b || a == c || b == c) {
+            System.out.println("Trojkat rownoramienny");
+        } else if (a + b < c || a + c < b || b + c < a) {
+            System.out.println("Trojkat nie istnieje");
+        } else {
+            System.out.println("Zwykly, nudny trojkat");
+        }
     }
-    /** Wyświetla ekran pomocy */
-    public static void pomoc(){
+
+    /**
+     * Wyświetla ekran pomocy
+     */
+    public static void pomoc() {
         System.out.println("Acme INC. (C) 2022");
-        System.out.println("Program do rozpoznawania rodzaju trójkąra");
+        System.out.println("Program do rozpoznawania rodzaju trójkąta");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
-    /** Glowna funkcja */
+
+    /**
+     * Glowna funkcja
+     */
     public static void main(String... args) {
         if (args.length != 3) {
             pomoc();
@@ -30,7 +44,7 @@ class Trojkaty {
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
-        
+
         jakiTrojkat(a, b, c);
     }
 }
